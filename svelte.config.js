12a5855f46c81d54d7224858d5fdab94a5786480
Alexander Russell <ajex94@gmail.com>
2026-02-22 20:38:38 -0800

    chore: remove base path configuration from Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		paths: {
-			// TODO: remove this when we have a domain name and can set up redirects
-			base: process.env.NODE_ENV === 'production' ? '/whits' : ''
 		},
 		adapter: adapter({
 			pages: 'build',
