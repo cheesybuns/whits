44dec010d2d01ab5d405eef472b6b4f09e211bd1
Alexander Russell <ajex94@gmail.com>
2026-02-22 20:22:06 -0800

    feat: add base path configuration for production environment in Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: {
+			// TODO: remove this when we have a domain name and can set up redirects
+			base: process.env.NODE_ENV === 'production' ? '/whits' : ''
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
